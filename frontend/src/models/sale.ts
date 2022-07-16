9eab079ad6d38b8bf5c030ff0b8903f00eed6e9b
= <=>
2022-07-15 23:35:53 -0300

    deploy

--- frontend/src/models/sale.ts
+++
@@ -1,8 +0,0 @@
-export type Sale = {
-    id: number;
-    sallerName: string;
-    visited: number;
-    deals: number;
-    amount: number;
-    date: string;
-}
--- frontend/src/utils/request.ts
+++
@@ -1,2 +0,0 @@
-export const BASE_URL = import.meta.env.VITE_BACKEND_URL ?? "http://localhost:8080";
-export const VITE_BACKEND_URL = "https://dsmeta-rafael.herokuapp.com"
